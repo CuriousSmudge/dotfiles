689b5730d4626dc2b08e4769adcddbe18690c219
CuriousSmudge <120012535+CuriousSmudge@users.noreply.github.com>
2024-08-29 10:16:07 +1000

    enola gay

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,7 @@ if status is-interactive
     alias lsa="ls -la"
     alias py="python3"
     alias n="nvim"
+    alias zed="flatpak run dev.zed.Zed"
     alias neovim="echo \"Wrong\""
 
     alias :q="echo \"okay if you are sure\" && sleep 1 && exit"
@@ -15,14 +16,17 @@ if status is-interactive
 
     export GTK_THEME=Adwaita:dark
     export XCURSOR_THEME=Bibata-Modern-Classic
+    export XCURSOR_SIZE=24
     # fix vscodium
     export ELECTRON_OZONE_PLATFORM_HINT=wayland
-
-
+    export QT_QPA_PLATFORMTHEME=qt5ct
+    export QT_QPA_PLATFORM=wayland
     # export PATH=$PATH:/home/d/.cargo/bin
     #
     fish_add_path /home/d/.cargo/bin
+    fish_add_path /home/d/bin/
+    fish_add_path /home/d/.local/share/gem/ruby/3.2.0/bin
+
 
 
 end
-pyenv init - | source
