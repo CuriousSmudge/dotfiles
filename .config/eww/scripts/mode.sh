c1b74fd1515d8b7add3f39ff3239f8f101e1ec0b
CuriousSmudge <120012535+CuriousSmudge@users.noreply.github.com>
2025-11-10 11:18:41 +1100

    start over

--- .config/eww/scripts/mode.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-swaymsg -m -t subscribe '["mode"]' |
-while read -r event_json; do
-    echo $(jq -r '.change' <<< $event_json)
-done
-
-# swaymsg -m -t SUBSCRIBE "[ 'mode' ]" | jq ".change"
--- .config/eww/scripts/window.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-swaymsg -m -t subscribe '["window"]' |
-while read -r event_json; do
-    echo $(jq -r '.container.name' <<< $event_json)
-done
